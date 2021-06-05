261252afeca25584dc7fc361cdb9ac21441cdd74
Buzzbyte <1598588+buzzbyte@users.noreply.github.com>
2021-06-05 14:15:11 -0400

    [package] Temp workaround for "pip install -e" bug * Temporary workaround for pip bug: pypa/pip#7953

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import site
+import sys
+
 import setuptools
+
+# temp workaround for bug mentioned in
+# https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 if __name__ == "__main__":
     setuptools.setup()
